c88337c7b24d4d9acb1cecb157d95245f0086061
WRHX <wouterhaaxman@gmail.com>
2017-12-23 01:35:05 +0100

    Bullet shooting
    Shooting bullets works now.
    Next: bullets going into the direction of the mouse position at the time it was clicked. This involves some math maybe.

--- freedom.py
+++ freedom.py
@@ -2,6 +2,10 @@
 Project initiation date: December 22 2017
 Author: Wouter Haaxman
 Regarding project code: Copyright 2017, Wouter Haaxman, All rights reserved.
+
+useful links (used/tweaked parts to implement here):
+https://www.pygame.org/docs
+
 '''
 
 import sys
@@ -17,13 +21,14 @@ class Soldier(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
         #Create soldier sprite.
-        self.image = pygame.image.load('ball_black.png').convert()
+        self.image = pygame.image.load('images/ball_black.png').convert() #image object is a surfaceS
 
-        #Blit the image surface on the game surface.
         self.rect = self.image.get_rect()
-        self.x = int(display_pixel_width/2)
-        self.y = int(display_pixel_height/2)
-        DISPLAYSURF.blit(self.image, (self.x, self.y))
+        self.rect.centerx = int(display_pixel_width/2)
+        self.rect.centery = int(display_pixel_height/2)
+
+    def update(self):
+        pass
 
 
 class Bullet(pygame.sprite.Sprite):
@@ -31,14 +36,17 @@ class Bullet(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
 
-        self.x = int(display_pixel_width/2)
-        self.y = int(display_pixel_height/2)
         #Create soldier sprite and resize it.
-        self.image = pygame.image.load('ball_black.png')#.convert()
+        self.image = pygame.image.load('images/ball_black.png')#.convert()
         self.size = self.image.get_size() # return a width and height of the image
-        self.image = pygame.transform.scale(self.image, (int(self.size[0]*0.05), int(self.size[1]*0.05)))
-        #Blit the image surface on the game surface.
-        DISPLAYSURF.blit(self.image, (self.x, self.y))
+        self.image = pygame.transform.scale(self.image, (int(self.size[0]*0.2), int(self.size[1]*0.2)))
+
+        self.rect = self.image.get_rect()
+
+
+    def update(self):
+        """ Move the bullet. """
+        self.rect.y -= 3
 
 #============================================================================
 
@@ -54,15 +62,20 @@ color_green = pygame.Color(0,255,0)
 color_blue = pygame.Color(0,0,255)
 
 #==============DEFINE DISPLAY SCREEN======================================
-DISPLAYSURF = pygame.display.set_mode((display_pixel_width,display_pixel_height))
+BACKGROUND_SURFACE = pygame.display.set_mode((display_pixel_width,display_pixel_height))
 pygame.display.set_caption('Freedom')
 
 #set background of the display surface to white and draw circle in middle
-DISPLAYSURF.fill(color_white)
+BACKGROUND_SURFACE.fill(color_white)
 
+#initialize bullet list which is update in event handler.
+all_sprites_list = pygame.sprite.Group()
+bullet_list = pygame.sprite.Group()
+#Add soldier and put in sprites list
 soldier1 = Soldier()
+all_sprites_list.add(soldier1)
 
-#pygame.draw.circle(DISPLAYSURF, color_black, (soldier1.x, soldier1.y), soldier1.pixel_radius, 0)
+#pygame.draw.circle(BACKGROUND_SURFACE, color_black, (soldier1.x, soldier1.y), soldier1.pixel_radius, 0)
 #=================MAIN GAME LOOP===============================================
 while True:
     #Event handler
@@ -70,6 +83,32 @@ while True:
         if event.type == QUIT:
             pygame.quit()
             sys.exit()
+        if event.type == MOUSEBUTTONDOWN:
+            #print(pygame.mouse.get_pos())
+            if pygame.mouse.get_pressed()[0] == 1: # (left mb,right mb, scroller?) -> (0/1,0/1,?)
+                bullet = Bullet()
+                # Set the bullet so it is where the player is
+                bullet.rect.x = soldier1.rect.x
+                bullet.rect.y = soldier1.rect.y
+                # Add the bullet to the lists
+                all_sprites_list.add(bullet)
+                bullet_list.add(bullet)
+                print('SHOOT')
+
+    all_sprites_list.update() # This is a nice feature to quickly call the update()
+    #method for all of the sprites in this list.
+
+    # Calculate bullet trajectories (to come soon)
+    for bullet in bullet_list:
+        # Remove the bullet when out of bounds
+        if bullet.rect.y < -10:
+            bullet_list.remove(bullet)
+            all_sprites_list.remove(bullet)
+
+
+    BACKGROUND_SURFACE.fill(color_white)# Clear the screen
+    all_sprites_list.draw(BACKGROUND_SURFACE)# Draw all the spites
+
     #Events handled and possibly game state changed: update display accordingly
-    pygame.display.update()
+    pygame.display.flip()# Go ahead and update the screen with what we've drawn.
     fpsClock.tick(FPS)
